fb9643025c31bbcaee260040c32fb22049cd54a2
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2017-10-20 22:48:34 -0700

    Creating the game window

--- snakeGame.py
+++ snakeGame.py
@@ -2,12 +2,19 @@ import pygame, sys, random, time
 
 
 def main():
+
     check_errors = pygame.init()
+
     if check_errors[1] > 0:
         print("There was an error in initialization")
         sys.exit(-1)
 
     else:
         print("Successfully initialized")
+
+    pygame.display.set_caption("Snake game.")
+    pygame.display.set_mode((720, 480))
+    time.sleep(5)
+
 
 main()
