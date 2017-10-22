5c106d9db5f65135dfb6af38d1234dfb389b2a64
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2017-10-21 22:58:51 -0700

    Changing directions and updating snake position

--- snakeGame.py
+++ snakeGame.py
@@ -31,22 +31,55 @@ direction = 'RIGHT'
 changeDirectionTo = direction
 
 while True:
+
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 changeDirectionTo = 'RIGHT'
-            if event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT:
                 changeDirectionTo = 'LEFT'
-            if event.key == pygame.K_UP:
+            elif event.key == pygame.K_UP:
                 changeDirectionTo = 'UP'
-            if event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_DOWN:
                 changeDirectionTo = 'DOWN'
-            if event.key == pygame.K_ESCAPE:
+            elif event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+
+    if changeDirectionTo == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
+    if changeDirectionTo == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if changeDirectionTo == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    if changeDirectionTo == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+
+    #changing the direction of the snake
+
+    if direction == 'RIGHT':
+        snake_position[0] += 10
+    if direction == 'LEFT':
+        snake_position[0] -= 10
+    if direction == 'UP':
+        snake_position[1] -= 10
+    if direction == 'DOWN':
+        snake_position[1] += 10
+
+    snake_body.insert(0, list(snake_position))
+
+    if snake_position[0] == food_position[0] and snake_position[1] == food_position[1]:
+        foodSpawned = False
+    else:
+        snake_body.pop()
+
+    if not foodSpawned:
+        food_position = [random.randrange(0, 72) * 10, random.randrange(0, 48) * 10]
+        foodSpawned = True
 
 
 
